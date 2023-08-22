e4a58554070b7f4b94ae57770411a169611b41a0
yaduvamsi1807 <141894946+yaduvamsi1807@users.noreply.github.com>
2023-08-22 22:36:37 +0530

    Add files via upload

---
+++ FIBONACCI SERIES.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int n,i,first=0,second=1,next;
+	printf("enter the number of terms: ");
+	scanf("%d", &n);
+	printf("fibonacci series: ");
+	for (i=0; i<n; i++){
+		if(i<=1)
+		next=i;
+		else{
+			next=first+second;
+			first=second;
+			second=next;
+		}
+		printf("%d", next);
+	}
+	return 0;
+	
+}
